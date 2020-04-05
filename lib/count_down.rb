e8f8adc446b589b500b3b656daad9dcf43c4c1e0
Ben Parish <parishben13@gmail.com>
2020-04-05 18:59:04 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,6 @@
 # Write your code here
-count = 10 
-while count >= 1 do
-count -= 1 
-break if count == 0 puts "Happy New Year!"
-puts count
-end
+ while count >=0 do
+055:1* count = count -1
+irb(main):056:1> puts count
+irb(main):057:1> break if count ==1
+irb(main):058:1> end
